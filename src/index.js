d63c225d643b40b24dff9b296fa07bd31c09fd93
= <=>
2025-11-28 21:18:12 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { LoginPage } from './Login/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/match" element={<BoardPage/>}></Route>
